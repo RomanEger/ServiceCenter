100806363f5a58b92403980b293cdb374f0902a6
ShihiroMori <shihiromori@mail.ru>
2024-06-08 00:44:45 +0600

    vvel soobshenie

---
+++ ServiceCenterApp/ServiceCenterApp/Views/Reports/ReportsCreate.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ServiceCenterApp.Views.Reports
+{
+    /// <summary>
+    /// Логика взаимодействия для ReportsCreate.xaml
+    /// </summary>
+    public partial class ReportsCreate : Page
+    {
+        public ReportsCreate()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDetailsReport_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnWorkReport_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnTimeReport_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnAnalytic_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ServiceCenterApp/ServiceCenterApp/Views/Requests/CreateRequest.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ServiceCenterApp.Views.Requests
+{
+    /// <summary>
+    /// Логика взаимодействия для CreateRequest.xaml
+    /// </summary>
+    public partial class CreateRequest : Page
+    {
+        public CreateRequest()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSendRequest_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnAddClient_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
